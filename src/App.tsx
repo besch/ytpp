299309365ad7ebd96cbdc65b488f8b8bb55ceb6b
vinog <vinogradskiy.n@gmail.com>
2024-11-28 14:18:51 +0100

    clean ups

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,8 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Navigation from "@/pages/Navigation";
 import TimelineList from "@/components/Timeline/TimelineList";
 import TimelineEditor from "@/components/Timeline/TimelineEditor";
-import { useCanvasEvents } from "./hooks/useCanvasEvents";
 
 const App: React.FC = () => {
-  useCanvasEvents();
-
   return (
     <div className="h-full flex overflow-hidden">
       <div className="w-full h-full flex flex-col bg-background text-foreground">
