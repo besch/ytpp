d224513216048b234fa1f263becb4150a721cf7e
vinog <vinogradskiy.n@gmail.com>
2024-12-18 15:21:43 +0100

    updates

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = "http://localhost:3000";
-// const API_BASE_URL = "https://video-timelines.vercel.app/";
+// const API_BASE_URL = "https://video-timelines.vercel.app";
 
 module.exports = {
   API_BASE_URL,
