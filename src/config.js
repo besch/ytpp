d39ae957009185e6f563178c99399639664c334a
vinog <vinogradskiy.n@gmail.com>
2024-12-28 21:45:38 +0100

    updates

--- src/config.js
+++ src/config.js
@@ -1,12 +1,6 @@
 // const API_BASE_URL = "http://localhost:3000";
 const API_BASE_URL = "https://video-timelines.vercel.app";
-const defaultOverlayDuration = 5;
-const defaultPauseDuration = 5;
-const defaultSkipDuration = 3000;
 
 module.exports = {
   API_BASE_URL,
-  defaultOverlayDuration,
-  defaultPauseDuration,
-  defaultSkipDuration,
 };
